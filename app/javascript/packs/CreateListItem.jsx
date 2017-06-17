428e4ccb16bad1221362bc6533a46f7c1065726a
vheimpel <vheimpel@gmail.com>
2017-06-17 18:22:59 +0000

    Adds materialize styles on nav bar and buttons

--- app/javascript/packs/CreateListItem.jsx
+++ app/javascript/packs/CreateListItem.jsx
@@ -15,7 +15,7 @@ class CreateListItem extends React.Component {
         <input type="name" onChange={this.updateName} placeholder="list-item name" />
         <input type="description" onChange={this.updateDescription}placeholder="list-item description" />
         <input type="link" onChange={this.updateLink} placeholder="list-item link" />
-        <button className="add-list-item" onClick={this.handleAddClick}>+</button>
+        <button className="add-list-item btn-floating btn-large waves-effect waves-light red" onClick={this.handleAddClick}>+</button>
 
       </div>
     )
