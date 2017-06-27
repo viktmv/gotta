9af0d927b2381d0d7f3ff391b27df728b63d9337
vheimpel <vheimpel@gmail.com>
2017-06-27 02:22:35 +0000

    Re-styles floating action button

--- app/javascript/index/ListItem.jsx
+++ app/javascript/index/ListItem.jsx
@@ -4,19 +4,28 @@ import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentRemove from 'material-ui/svg-icons/content/remove';
 
 const style = {
-  marginLeft: 10,
-  verticalAlign: 30,
-  cssFloat: "right",
-  marginRight: 30
+  // width: 30,
+  // height: 30,
 };
 
 class ListItem extends React.Component {
   render() {
     return (<div id={this.props.data.itemKey} className="list-item" key={this.props.data.itemKey}>
-              <div className="item-image" style={{ backgroundImage: `url(${this.props.data.img})`, width: '72px', height: '72px'}}></div>
-              <b className="list-item-name">{this.props.data.name}</b><br />
-              <span className="list-item-description">{this.props.data.description}<a href={this.props.data.link} className="list-item-link">...More</a></span>
-              <FloatingActionButton mini={true} style={style} className="rm-list-item" onClick={this.props.handleRemoveClick}><ContentRemove /></FloatingActionButton>
+
+              <div className="image-container">
+                <div className="item-image" style={{ backgroundImage: `url(${this.props.data.img})`, width: '72px', height: '72px'}}></div>
+              </div>
+
+              <div className="list-body-container">
+                <b className="list-item-name">{this.props.data.name}</b><br />
+                <span className="list-item-description">{this.props.data.description}<a href={this.props.data.link} className="list-item-link">
+                ...More</a></span>
+              </div>
+
+              <div className="list-button-container">
+                <FloatingActionButton mini={true} className="rm-list-item" onClick={this.props.handleRemoveClick}><ContentRemove /></FloatingActionButton>
+              </div>
+
             </div>)
   }
 }
